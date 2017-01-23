9f70aa08e6bd65d12de17cec6c58b9936ea5713e
62fe50 <merrickslane@gmail.com>
2017-01-22 23:06:27 -0800

    commented things

--- Processor/VM.c
+++ Processor/VM.c
@@ -12,10 +12,10 @@ void fetch(void)
 
 void decode(void)
 {
-	opcode = (IR[0] >> 11)&0x1f;
-	r1 = (IR[0] >> 8)&0x07;
-	r2 = (IR[0] >> 5)&0x07;
-	subop = IR[0]&0x1f;
+	opcode = (IR[0] >> 11)&0x1f; //highest 5 bits
+	r1 = (IR[0] >> 8)&0x07; //next 3 bits
+	r2 = (IR[0] >> 5)&0x07; //next 3 bits
+	subop = IR[0]&0x1f; //last 5 bits
 	
 	if (wordmode == 2) { //if two-word instruction
 		immediate = IR[1];
